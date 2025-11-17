60676887f8d47f1a27b5d5f0ad83bf5b60205bb9
Irina Sharapova <ir.ur.smirnova@gmail.com>
2025-11-17 14:03:07 -0500

    add simple function kata

--- 8kyu/arrays.js
+++ 8kyu/arrays.js
@@ -3,3 +3,28 @@ let right = [3, 2, 1, 2];
 let leftFromRight = right.reverse();
 console.log (leftFromRight);
 
+// 1.1 flip function
+const flip =(d,a) => {
+    if (d === "R") {
+        return a.sort((x,y)=> x-y);
+    };
+    if (d ==="L") {
+        return a.sort((x,y)=> y-x);
+    }
+    
+    
+}
+console.log(flip("R", [2, 3, 4, 5, 6]));
+console.log(flip("L", [3, 2, 1, 2]));
+
+// 2. Summ array
+function sum (numbers) {
+  return numbers.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+}
+let numbers = [3, 0, 4, 1];
+console.log (sum (numbers));
+// 3. Function simple 
+function greet () {
+  return "hello world!";
+  }
+  console.log(greet ());gir 
